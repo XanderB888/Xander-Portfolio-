f27a35bd4494142a3a3169b832d14632e011462c
Xander <xander.cage.botha@gmail.com>
2024-08-24 17:17:36 +0100

    add quote generator html and JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Color changer project start
 const flipper = document.getElementById("xxx")
 
 function setColor(name) {
@@ -12,3 +13,40 @@ function randomColor () {
     const color = `rgb(${red}, ${green}, ${blue})`
     xxx.style.color = color;
 }
+//Color changer project end
+
+//Quote generator start
+const quotes = ["I played ice hockey for South Africas Senior Mens National team, and I was also the assistant captain", 
+    "I grew up hunting with my dad and brother",
+     "I always want to spoil the ones I love", 
+     "I previously lived in Boston for a year", 
+     "I previously lived in Bradford, Canada for a year", 
+     "I love all forms of nature and wildlife", 
+     "I try to always maintain a good level of fitness", 
+     "I love my little french bulldog 'Loki'"]
+
+    const usedIndexes = new Set()
+    const quoteElement = document.getElementById("quote")
+
+    function generateQuote() {
+        if (usedIndexes.size >= quotes.length) {
+            usedIndexes.clear()
+        }
+
+        while (true) {
+            const randomIdx = Math.floor(Math.random() * quotes.length)
+
+            if (usedIndexes.has(randomIdx)) continue
+
+            const quote = quotes[randomIdx]
+            quoteElement.innerHTML = quote;
+            usedIndexes.add(randomIdx)
+            break 
+        }
+        
+    }
+
+
+
+
+//Quote generator end
