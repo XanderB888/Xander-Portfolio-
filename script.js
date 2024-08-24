9f3e2b84941908a835a9ca07182ed1be1e9234ee
Xander <xander.cage.botha@gmail.com>
2024-08-24 16:29:26 +0100

    Add buttons with working JS

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
+const flipper = document.getElementById("xxx")
 
+function setColor(name) {
+    xxx.style.color = name;
+}
 
+function randomColor () {
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    xxx.style.color = color;
+}
